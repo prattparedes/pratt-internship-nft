f86a01a7ecfa42ecdb62cd72994b9a77a7df3a8c
Pratt <pratt.paredes@gmail.com>
2022-11-29 23:37:03 -0500

    added newitems and countdown

---
+++ src/components/UI/Countdown.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+function Countdown({ date }) {
+  const [hours, setHours] = useState();
+  const [minutes, setMinutes] = useState();
+  const [seconds, setSeconds] = useState();
+
+  useEffect(() => {
+    let intervalId = setInterval(() => updateTimer(), 1000);
+
+    return () => {
+      window.clearInterval(intervalId);
+    };
+  }, []);
+
+  function updateTimer() {
+    let countdown = (date - Date.now()) / 1000
+
+    let hours = Math.floor(countdown / 3600)
+    setHours(hours)
+
+    let minutes = Math.floor(countdown / 60) % 60
+    setMinutes(minutes)
+
+    let seconds = Math.floor(countdown % 60)
+    setSeconds(seconds)
+  }
+
+  return <div className="de_countdown">{hours}h {minutes}m {seconds}s</div>;
+}
+
+export default Countdown;
